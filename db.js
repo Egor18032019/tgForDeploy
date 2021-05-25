9c4b51fc194eaec27a19843ed6229251ef7147e8
Egor18032019 <goro4@mail.ru>
2021-05-25 10:40:54 +0500

    добавил бд на Heroku

--- db.js
+++ db.js
@@ -3,12 +3,14 @@ const {
 } = require('sequelize');
 
 module.exports = new Sequelize(
-  "tg_bot", // Название БД
-  "postgres", // Пользователь
-  "222324", // ПАРОЛЬ
+  "dc484he6p1vgo5", // Название БД
+  "dhqqmudfzhudws", // Пользователь
+  "51afaf26ea47a9e48497c8273cfac16b078e58067f18ddc519b55deb3ff58946", // ПАРОЛЬ
   {
+    host: "ec2-54-155-208-5.eu-west-1.compute.amazonaws.com",
     dialect: 'postgres',
-    host: "localhost",
-    port: 5000,
+    port: 5432,
   }
 )
+
+// URI postgres://dhqqmudfzhudws:51afaf26ea47a9e48497c8273cfac16b078e58067f18ddc519b55deb3ff58946@ec2-54-155-208-5.eu-west-1.compute.amazonaws.com:5432/dc484he6p1vgo5
